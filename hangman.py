5698ad1aaac4b695ab3ec2e82d7504b78c7d60ab
Abby-Dhone-1819 <43389639+Abby-Dhone-1819@users.noreply.github.com>
2019-03-15 14:17:28 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -8,20 +8,22 @@ import time
 window = turtle.Screen()
 window.bgcolor("Dim Gray")
 
- #creates the hangman board and in correct letter box and explains the game
+ #creates the hangman board and gives the color pen size
 hang = turtle.Turtle()
 hang.color("Medium Turquoise")
 hang.pensize(6)
 hang.shape("classic")
 
+#This is the SECOND turtle for drawing the body
 hangs = turtle.Turtle()
 hangs.color("Medium Turquoise")
 hangs.pensize(6)
 hangs.shape("classic")
 
 
-#turtle info for board
+#turtle info for making the hangboard
 def hangBoard():
+    hang.speed(0)
     hang.hideturtle()
     hang.back(200)
     hang.forward(100)
@@ -31,14 +33,17 @@ def hangBoard():
     hang.forward(100)
     hang.right(90)
     hang.forward(30)
+    hangs.hideturtle()
     hangs.penup()
     hangs.left(90)
     hangs.forward(220)
     hangs.left(180)
     hangs.pendown()
 hangBoard()
-#code for body
+#Uses code to make the body after each incorrect guess
 def man():
+#This makes the head
+   hangs.speed(7)
    hangs.right(90)
    hangs.circle(30)
    hangs.penup()
@@ -46,46 +51,51 @@ def man():
    hangs.forward(60)
    time.sleep(2)
 def manbody():
+#Makes the body
    hangs.pendown()
    hangs.forward(80)
    hangs.back(70)
    time.sleep(2)
 def manleftarm():
+#Makes the left arm
    hangs.left(45)
    hangs.forward(50)
    hangs.back(50)
 def manrightarm():
+#Makes right arm
    hangs.right(90)
    hangs.forward(50)
    hangs.back(50)
 def manleftleg():
+#Makes left leg
    hangs.left(45)
    hangs.forward(70)
    hangs.left(45)
    hangs.forward(50)
    hangs.back(50)
 def manrightleg():
+#Makes right leg
    hangs.right(90)
    hangs.forward(50)
    hangs.back(50)
 
-
-words = 'ant salmon zebra'.split()
+#This is the word list
+words = 'ant salmon zebra'.split() #Split strings the words into a list
 
 def getRandomWord(wordList):
-    # This function returns a random string from the passed list of strings.
+# This function returns a random string from the passed list of strings.
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
+#This code sets up the missed letter count
 def displayBoard(missedLetters, correctLetters, secretWord):
     print([len(missedLetters)])
     print()
-
     print('Missed letters:', end=' ')
     for letter in missedLetters:
         print(letter, end=' ')
     print()
-
+#Sets up the blanks
     blanks = '_' * len(secretWord)
 
     for i in range(len(secretWord)): # replace blanks with correctly guessed letters
@@ -116,13 +126,14 @@ def playAgain():
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
-
+#Resets missed letters and the correct letters as well as the random word
 print('H A N G M A N')
 missedLetters = ''
 correctLetters = ''
 secretWord = getRandomWord(words)
 gameIsDone = False
 
+#Ture keeps the game runing
 while True:
     displayBoard(missedLetters, correctLetters, secretWord)
 
@@ -137,14 +148,16 @@ while True:
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
                 foundAllLetters = False
-                break
+                break #Stops first loop
+        #Once all letters, it reveals the secert word and tell player they won. Then it takes them to 'gameIsDone.'
         if foundAllLetters:
             print('Yes! The secret word is "' + secretWord + '"! You have won!')
             gameIsDone = True
+    # If guessed incorrectly it directs them to 'missedLetters'
     else:
         missedLetters = missedLetters + guess
 
-        # Check if player has guessed too many times and lost
+        #If one miss occures, a body part will be drawn. Once all 6 body parts are done, game is done
         if len(missedLetters) == 7:
             displayBoard(missedLetters, correctLetters, secretWord)
         if len(missedLetters)==2-1:
@@ -159,7 +172,9 @@ while True:
             manleftleg()
         if len(missedLetters)==7-1:
             manrightleg()
+            # Check if player has guessed too many times and lost
             print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
+
             gameIsDone = True
 
 
@@ -173,7 +188,7 @@ while True:
             gameIsDone = False
             secretWord = getRandomWord(words)
         else:
-            break
-
+            break #This break ends the game completely
+#Keeps turtle window open
 tkinter.mainloop()
 
